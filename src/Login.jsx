9475875518d1ecbadd6a28b3e81496bdd8ab4847
kumaresan2001 <kumaresancrseven@gmail.com>
2023-02-26 12:43:31 +0530

    day32task

---
+++ src/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Portal from "./Portal";
+import Dashboard from "./Dashboard";
+
+function Login() {
+  return (
+    <div>
+      <Portal />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default Login;
